e5ea333abf1c5b4ea8b4d64cc3d992cd0db6b3bf
unknown <mohammedabakrim@gmail.com>
2023-11-21 12:07:53 +0100

    working a little bit with the tailwind config file creating our custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}",
-    '*.{html,js}'],
+  '*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#FF6363",
+        secondary:{
+          100:'#E2E2D5',
+          200:'#888883'
+        }
+      }
+    },
   },
   plugins: [],
 }
